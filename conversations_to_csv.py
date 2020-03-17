882e53cc22eb1d76508c7cdf37a0949f1a83fc19
Zuzanna <zuzanna.deutschman@gmail.com>
2020-03-17 20:54:22 +0100

    print clean messages

--- conversations_to_csv.py
+++ conversations_to_csv.py
@@ -14,34 +14,26 @@ import numpy as np
 import itertools
 import networkx as nx
 import collections
-#import igraph
+import re
     
-    
-#contents = open("MyFriend_YJX3MkSiqQ/message_1.html", encoding="utf-8").read()
-#soup = BeautifulSoup(contents, 'lxml')
-#lists = soup.find_all("div", {"class": "pam _3-95 _2pi0 _2lej uiBoxWhite noborder"})
-#for lis in lists: 
-#    names = lis.find_all("div", {"_3-96 _2pio _2lek _2lel"}) 
-#    print(names)
-#    entries = lis.find_all("div", {"class": "_3-96 _2let"}) 
-#    for entry in entries: 
-#        texts = entry.find_all("div")
-#        for item in texts: 
-#            string = item.find_all("div") 
-#            print(string)
 
-contents = open("MyFriend_YJX3MkSiqQ/message_1.html", encoding="utf-8").read()
-soup = BeautifulSoup(contents, 'lxml')
-lists = soup.select(".pam._3-95._2pi0._2lej.uiBoxWhite.noborder")
-for lis in lists: 
-    name = lis.select("._3-96._2pio._2lek._2lel")[0].getText() 
-    print(name)
-    entries = lis.find_all("div", {"class": "_3-96 _2let"}) 
-    for entry in entries: 
-        texts = entry.find_all("div")
-        for item in texts: 
-            string = item.find("div", text=True)#.getText()
-            if string != None:
-                string = string.getText()
-                
-            print(string)
+for file_number in range (1, 0, -1):
+    contents = open(f"MyFriend_YJX3MkSiqQ/message_{file_number}.html", encoding="utf-8").read()
+    soup = BeautifulSoup(contents, 'lxml')
+    lists = soup.select(".pam._3-95._2pi0._2lej.uiBoxWhite.noborder")
+    temp_name = lists[0].select("._3-96._2pio._2lek._2lel")[0].getText()
+    print(temp_name)
+    for lis in reversed(lists): 
+        name = lis.select("._3-96._2pio._2lek._2lel")[0].getText() 
+        entries = lis.find_all("div", {"class": "_3-96 _2let"}) 
+        for entry in entries: 
+            texts = entry.find_all("div")
+            for item in texts: 
+                string = item.find("div", text=True)#.getText()
+                if string != None:
+                    string = string.getText()
+                    if not re.match("http.*", string): 
+                        if name != temp_name:
+                            print(name)
+                            temp_name = name
+                        print(string)
