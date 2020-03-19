b9072d4e091af77c69ce8e5516920eda674d66d0
Zuzanna <zuzanna.deutschman@gmail.com>
2020-03-19 22:03:31 +0100

    save inputs and outputs separately

--- conversations_to_csv.py
+++ conversations_to_csv.py
@@ -15,25 +15,56 @@ import itertools
 import networkx as nx
 import collections
 import re
-    
 
-for file_number in range (1, 0, -1):
+    
+def selectFileToSave(name):
+    if re.match('Zuzanna', name):
+        file = 'input_text.txt'
+    else:
+        file = 'output_text.txt'
+    return file
+
+
+def input_and_output(line_number):
+    input_txt = open('input_text.txt', encoding="utf-8")
+    all_inputs = input_txt.readlines()
+    #print(f"{all_inputs[line_number]}")
+    output_txt = open('output_text.txt', encoding="utf-8")
+    all_outputs = output_txt.readlines()
+    #print(f"{all_outputs[line_number]}")
+    return all_inputs[line_number], all_outputs[line_number]
+
+#with open('input_text.txt', 'a') as input_text, open('output_text.txt', 'a') as output_text:
+    
+for file_number in range (19, 0, -1):
     contents = open(f"MyFriend_YJX3MkSiqQ/message_{file_number}.html", encoding="utf-8").read()
     soup = BeautifulSoup(contents, 'lxml')
     lists = soup.select(".pam._3-95._2pi0._2lej.uiBoxWhite.noborder")
     temp_name = lists[0].select("._3-96._2pio._2lek._2lel")[0].getText()
+    file = selectFileToSave(temp_name)
     print(temp_name)
     for lis in reversed(lists): 
         name = lis.select("._3-96._2pio._2lek._2lel")[0].getText() 
         entries = lis.find_all("div", {"class": "_3-96 _2let"}) 
+        
         for entry in entries: 
             texts = entry.find_all("div")
             for item in texts: 
                 string = item.find("div", text=True)#.getText()
                 if string != None:
                     string = string.getText()
-                    if not re.match("http.*", string): 
-                        if name != temp_name:
-                            print(name)
-                            temp_name = name
-                        print(string)
+                    if not re.match("http.*", string):
+                        with open(file, 'a', encoding = "utf-8", newline='') as f:
+                            if name != temp_name:
+                                f.write('\n')
+                                file = selectFileToSave(name)
+                                temp_name = name
+                            print(string)
+                            f.write(string.strip("\n") + ' ')
+                            
+
+                            
+num_entry_lines = sum(1 for line in open('input_text.txt', encoding="utf-8"))
+print(num_entry_lines)
+num_output_lines = sum(1 for line in open('output_text.txt', encoding="utf-8"))
+print(num_output_lines)
